60f45e94d0e310ed32ab7eb8fb0893d779128771
ThoFur <Thomas.Furniere@vdabcampus.be>
2023-08-29 10:05:01 +0200

    Voorbeeld1

---
+++ Voorbeeld1/Controllers/FiliaalController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Voorbeeld1.Models;
+
+namespace Voorbeeld1.Controllers
+{
+    public class FiliaalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
